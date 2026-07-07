820cf0f81408f2fbf5f9b050e7602d43e6c58fe0
system-intelligence <system.intelligence2025@gmail.com>
2026-07-07 16:24:47 +0800

    put back the edit svg beside the publish btn

--- resources/views/components/icon.blade.php
+++ resources/views/components/icon.blade.php
@@ -172,6 +172,10 @@ $icons = [
 <circle cx="12" cy="17" r=".5" fill="currentColor" stroke="none" />
 ',
 
+'pencil' => '
+<path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Z" />
+',
+
 'share' => '
 <circle cx="18" cy="5" r="2" />
 <circle cx="6" cy="12" r="2" />
